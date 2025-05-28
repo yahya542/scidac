49b86fc831e834efd22ad97467ceb9d014d788d9
yahya <muhyahyaas@gmail.com>
2025-05-28 11:12:29 +0700

    delete apps.py

--- myapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class YourAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp'
-
-    def ready(self):
-        import myapp.signals  
